32b6a33630ac72e5c7185a15ca2ec647dec11752
Wesley Anastasi <anastasicrl@gmail.com>
2023-02-03 22:23:35 -0600

    updated documentation

--- Mp1/mp1.cpp
+++ Mp1/mp1.cpp
@@ -9,19 +9,33 @@
 
 /*
 * Data Abstraction:
+    input file is opened
+    floats sum and input are declared
+    int chargesNum is initialized
+    float array charges is created with size chargesNum
+    boolean swap is initialized
 * 
 * Input:
+    the program reads floats from the provided input file
+    and stores them in charges
 * 
 * Process:
+    charges is sorted in descending order, and then the sum is
+    taken of all the nums in the array
 * 
 * Output:
+    the sum is output
 *
 * Assumptions:
+    the inputFile opens
+    the input is correct
+    the user wants the sum of the nums in the file
 * 
 * Questions:
 * 1: Total charge = 50545.47
 * 2: Total charge ascending = 50545.19
 * 3: Total charge descending = 50545.37
+
 * 4: Because of the way rounding works, numbers get truncated when they get to big.
 * Because of this, adding together numbers in different orders can lead to different
 * parts being truncated which results in slightly different answers. The most
@@ -39,6 +53,7 @@ using namespace std;
 
 int main()
 {
+    //Data Abstraction
     ifstream inputFile;
     float sum, input;
     int chargesNum = 10000;
@@ -46,13 +61,17 @@ int main()
     bool swap;
 
     sum = 0;
+
+    //Open input file
     inputFile.open("charges.txt");
 
+    //Get input
     for (int i = 0; inputFile >> input; i++)
     {
         charges[i] = input;
     }
 
+    //Data Processing and Output
     do
     {
         swap = false;
@@ -74,5 +93,8 @@ int main()
     }
 
     cout << fixed << setprecision(2) << sum << endl;
+
+    //Close file
+    inputFile.close();
     return 0;
 }
